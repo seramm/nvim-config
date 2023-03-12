b622c6f321a01579f21b50acd08bc14e163498d5
Sergio Aramburu <sergioaramburu@tutanota.com>
2023-03-12 19:57:24 +0100

    Installed Which Key

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -49,6 +49,9 @@ return require('packer').startup(function(use)
   use 'folke/tokyonight.nvim'
   use 'goolord/alpha-nvim'
 
+  -- Help
+  use 'folke/which-key.nvim'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
