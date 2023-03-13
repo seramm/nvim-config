bc328564c51f9f4abc148502521e5f93b610d03b
Sergio Aramburu <sergioaramburu@tutanota.com>
2023-03-13 18:47:33 +0100

    Installed Tree Sitter

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -52,6 +52,9 @@ return require('packer').startup(function(use)
   -- Help
   use 'folke/which-key.nvim'
 
+  --Syntax
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
