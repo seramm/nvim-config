dd2cb7e54318d64ee269ba2429e21fb8769ae030
Sergio Aramburu <sergioaramburu@tutanota.com>
2023-03-13 16:40:00 +0100

    Added basic conf for Which Key

--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
 require "user.keymaps"
 require "user.options"
 require "user.plugins"
+-- Plugins config
 require "user.alpha"
+require "user.whichkey"
---
+++ lua/user/whichkey.lua
@@ -0,0 +1,92 @@
+-- Which Key config
+
+local status_ok, which_key = pcall(require, "which-key")
+if not status_ok then
+  return
+end
+
+local setup = {
+  plugins = {
+    marks = true,
+    registers = true,
+    spelling = {
+      enabled = true,
+      suggestions = 20,
+    },
+    presets = {
+      operators = false,
+      motions = false,
+      text_objects = false,
+      windows = true,
+      nav = true,
+      z = true,
+      g = true,
+    },
+  },
+  key_labels = {
+    -- ["<space>"] = "SPC",
+    -- ["<cr>"] = "RET",
+    -- ["<tab>"] = "TAB",
+    ["<leader>"] = "SPC",
+  },
+  icons = {
+    breadcrumb = "»",
+    separator = "",
+    group = "+",
+  },
+  popup_mappings = {
+    scroll_down = '<c-d>',
+    scroll_up = '<c-u>',
+  },
+  window = {
+    border = "rounded", -- none, single, double, shadow
+    position = "bottom", -- bottom, top
+    margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+    padding = { 1, 2, 1, 2 }, -- extra window padding [top, right, bottom, left]
+    winblend = 0
+  },
+  layout = {
+    height = { min = 4, max = 25 }, -- min and max height of the columns
+    width = { min = 20, max = 50 }, -- min and max width of the columns
+    spacing = 3, -- spacing between columns
+    align = "left", -- align columns left, center or right
+  },
+  ignore_missing = true,
+  hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ "},
+  show_help = false,
+  show_keys = false,
+  triggers = "auto",
+  triggers_blacklist = {
+    i = { "j", "k" },
+    v = { "j", "k" },
+  },
+  disable = {
+    buftypes = {},
+    filetypes = {},
+  },
+}
+
+local opts = {
+  mode = "n", -- NORMAL mode
+  prefix = "<leader>",
+  buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+  silent = true, -- use `silent` when creating keymaps
+  noremap = true, -- use `noremap` when creating keymaps
+  nowait = false, -- use `nowait` when creating keymaps
+}
+local mappings = {
+  s = {
+    name = "Splits",
+    v = { "<cmd>vsplit<cr>", "Vertical Split" },
+    h = { "<cmd>split<cr>", "Horizontal Split" },
+  },
+  w = {
+    name = "Write",
+    w = { "<cmd>w<cr>", "Save file" },
+    a = { "<cmd>wa<cr>", "Save all files" },
+  },
+}
+
+
+which_key.setup(setup)
+which_key.register(mappings, opts)
