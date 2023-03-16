88093502ea946b76819585cc566f44a91e03eb04
Sergio Aramburu <sergioaramburu@tutanota.com>
2023-03-16 15:47:25 +0100

    Added icons for diagnostics in the sign column

--- lua/user/lsp/lspconfig.lua
+++ lua/user/lsp/lspconfig.lua
@@ -52,3 +52,9 @@ nvim_lsp.lua_ls.setup({
 		},
 	},
 })
+
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
