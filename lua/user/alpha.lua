190287d16aadf52cb902ec24888a8659afba01ed
Sergio Aramburu <sergioaramburu@tutanota.com>
2023-03-17 21:32:46 +0100

    Added option at start(Alpha)

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -28,7 +28,8 @@ local header0 = {
 dashboard.section.header.val = header0
 dashboard.section.buttons.val = {
   dashboard.button("e", "  >  New file", ":ene <BAR> starinsert <CR>"),
-  dashboard.button("q", "󰅚  >  Quit NeoVim", ":qa<CR>"),
+  dashboard.button("ff","  >  Search file", "<cmd>Telescope find_files<CR>"),
+  dashboard.button("q", "󰅚  >  Quit NeoVim", "<cmd>qa<CR>"),
 
 }
 
