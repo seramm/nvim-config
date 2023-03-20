a64007eeaa6a44f835a3da506df235707b3a4050
Sergio Aramburu <sergioaramburu@tutanota.com>
2023-03-20 19:52:56 +0100

    Fix for minor mistake

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -83,7 +83,7 @@ return {
       require "user.lsp.cmp"
     end,
     event = {"InsertEnter", "CmdlineEnter"},
-     dependecies = {
+     dependencies = {
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-path',
