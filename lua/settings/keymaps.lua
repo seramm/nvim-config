e18e61db4ae12dce558a71ed9dbd714f0b1a8801
Sergio Aramburu <sergioaramburu@tutanota.com>
2023-03-21 19:47:13 +0100

    Added keybinds to move around buffers

--- lua/settings/keymaps.lua
+++ lua/settings/keymaps.lua
@@ -12,3 +12,9 @@ vim.api.nvim_set_keymap("n", "<C-Space>", "<cmd>WhichKey<cr>", opts)
 -- Exit insert mode
 vim.api.nvim_set_keymap("i", "jk", "<Esc>", opts)
 vim.api.nvim_set_keymap("i", "kj", "<Esc>", opts)
+
+-- Buffer navigation
+vim.api.nvim_set_keymap("n", "<S-h>", "<cmd>bprevious<cr>", opts)
+vim.api.nvim_set_keymap("n", "<S-k>", "<cmd>bprevious<cr>", opts)
+vim.api.nvim_set_keymap("n", "<S-l>", "<cmd>bnext<cr>", opts)
+vim.api.nvim_set_keymap("n", "<S-j>", "<cmd>bnext<cr>", opts)
