ecf2c311827dacdfdf4de917e88ea2b4ef9fe59b
Sergio Aramburu <sergioaramburu@tutanota.com>
2023-10-24 11:26:22 +0200

    Scope highlight and remove Rainbow indent

--- lua/syntax/blankline.lua
+++ lua/syntax/blankline.lua
@@ -3,29 +3,14 @@ if not status_ok_1 then
   return
 end
 
-local highlight = {
-  "RainbowRed",
-  "RainbowYellow",
-  "RainbowGreen",
-  "RainbowCyan",
-  "RainbowBlue",
-  "RainbowViolet",
-}
-
-local hooks = require("ibl.hooks")
-
-hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-  vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-  vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-  vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-  vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
-  vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-  vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-end)
-
 local config = {
-  indent = {
-    highlight = highlight,
+  scope = {
+    enabled = true,
+    show_start = true,
+    show_end = true,
+    injected_languages = true,
+    highlight = { "Function", "Label" },
+    priority = 500,
   },
 }
 
