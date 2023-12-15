84c0cc02f20aecf546c739c7d7faec45b712864b
Sergio Aramburu <sergioaramburu@tutanota.com>
2023-12-15 13:49:45 +0100

    feat(LSP): new keybind for formatting

--- lua/settings/keymaps.lua
+++ lua/settings/keymaps.lua
@@ -35,3 +35,7 @@ vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
 vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", { silent = true, noremap = true })
 vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", { silent = true, noremap = true })
 vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", { silent = true, noremap = true })
+
+
+-- Null-ls format
+vim.keymap.set("n", "gt", "<cmd>NullFormat<cr>", opts)
