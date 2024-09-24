7c44317cf82c792c33cb0dd693ab7ad2ef0e370e
seramm <sergioaramburu@tutanota.com>
2024-09-24 14:32:57 +0200

    update: telescope 0.1.8

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -29,7 +29,7 @@ return {
     "nvim-telescope/telescope.nvim",
     lazy = true,
     cmd = "Telescope",
-    tag = "0.1.5",
+    tag = "0.1.8",
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
